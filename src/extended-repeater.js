01a47933129d8a007499c0ef707cb9d80a44c73f
vanzo16 <vanzo16@ya.ru>
2021-03-15 16:54:08 +0300

    add 2 tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,3 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+const chorus = 'String repeat ';
+console.log(`Answer : ${chorus.repeat(7)}`);
   
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,15 @@
-const CustomError = require("../extensions/custom-error");
+resolver_tower_of_hanoi = function (tower1, tower2, tower3, n) {
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+    // terminating condition
+    if (n <= 0) {
+        return
+    }
+
+    resolver_tower_of_hanoi(tower1, tower3, tower2, n - 1)
+
+    console.log(`Move Disk-${n} FROM ${tower1} TO ${tower2}`);
+
+    resolver_tower_of_hanoi(tower3, tower2, tower1, n - 1);
+}
+
+resolver_tower_of_hanoi('tower1', 'tower2', 'tower3', 3)

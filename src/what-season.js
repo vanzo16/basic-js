64e87cbde5e5c8e31a74b58dbd9e9ea72e573891
vanzo16 <vanzo16@ya.ru>
2021-03-16 10:24:16 +0300

    add season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,50 @@
-const CustomError = require("../extensions/custom-error");
+function getSeason() {
+  month = document.forms.date.month.value.toLowerCase();
+  season = 'unknown';
+  switch(month) {
+      case 'dec':
+      case 'december':
+      case '12':
+      case 'jan':
+      case 'january':
+      case '1':
+      case 'feb':
+      case 'february':
+      case '2':
+          season = 'winter';
+      break;
+      case 'mar':
+      case 'march':
+      case '3':
+      case 'apr':
+      case 'april':
+      case '4':
+      case 'may':
+      case '5':
+          season = 'spring';
+      break;
+      case 'jun':
+      case 'june':
+      case '6':
+      case 'jul':
+      case 'july':
+      case '7':
+      case 'aug':
+      case 'august':
+      case '8':
+          season = 'summer';
+      break;
+      case 'sep':
+      case 'september':
+      case '9':
+      case 'oct':
+      case 'october':
+      case '10':
+      case 'nov':
+      case 'november':
+      case '11':
+          season = 'autumn';
+      break;
+  }
+}
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
